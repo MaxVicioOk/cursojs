b565dace19ccb0d02e79cc56fbefb9db7d221a73
Max Vicio <98363423+MaxVicioOk@users.noreply.github.com>
2022-07-27 16:39:02 -0300

    Desafio compl DOM

--- js/main.js
+++ js/main.js
@@ -1,37 +1,42 @@
 const carrito = []
-let precioDelCarrito = 0
-/* const fancyProduct = {id: 1, name: 'Fancy Product', price: 80}
-const specialItem = {id: 2, name: 'Special Item', price: 18}
-const saleItem = {id: 3, name: 'Sale Item', price: 25}
-const popularItem = {id: 4, name: 'Popular Item', price: 40} */
+const fancyProduct = document.querySelectorAll('.fancy-product')
+const specialItem = document.querySelectorAll('.special-item')
+const saleItem = document.querySelectorAll('.sale-item')
+const popularItem = document.querySelectorAll('.popular-item')
+const productos = [
+    {id: 1, name: 'Fancy Product', price: 80},
+    {id: 2, name: 'Special Item', price: 18},
+    {id: 3, name: 'Sale Item', price: 25},
+    {id: 4, name: 'Popular Item', price: 40}
+]
 
 function agregarAlCarrito(producto) {
-    let precio = ''
-    const nombre = prompt('Indique su nombre')
+    let nombre = prompt('Indique su nombre')
     let stock = parseInt(prompt('Numero para stock'))
     let compra = parseInt(prompt('Unidades que quiere comprar'))
-
-    if ((stock > compra) && (compra > 0)){
+    if ((stock >= compra) && (compra > 0)){
         if (producto == 'Fancy Product'){
-            carrito.push({id: 1, name: 'Fancy Product', price: 80})
+            carrito.push(productos[0])
         }
         else if (producto == 'Special Item'){
-            carrito.push({id: 2, name: 'Special Item', price: 18})
+            carrito.push(productos[1])
         }
         else if (producto == 'Sale Item'){
-            carrito.push({id: 3, name: 'Sale Item', price: 25})
+            carrito.push(productos[2])
         }
         else if (producto == 'Popular Item'){
-            carrito.push({id: 4, name: 'Popular Item', price: 40})
+            carrito.push(productos[3])
         }
         console.log(nombre + ', gracias por comprar ' + compra + ' unidades de ' + producto)
         const carritoParaConsola = []
         for (const objetoEnCarrito of carrito){
-                carritoParaConsola.push(objetoEnCarrito.name)
+            carritoParaConsola.push(objetoEnCarrito.name)
         }
-        console.log(carritoParaConsola)
-            for (const precio of carrito){
-            precioDelCarrito += precio.price*compra
+        console.log('En su carrito: ' + carritoParaConsola)
+
+        let precioDelCarrito = 0
+        for (const precio of carrito){
+            precioDelCarrito += (precio.price*compra)
         }
         console.log('El monto total de su compra es de: $' + precioDelCarrito)
     }
@@ -44,30 +49,37 @@ function agregarAlCarrito(producto) {
         }
     }
 }
-/*     switch (producto) {
-        case 'Fancy Product': 
-            precio = 80;
-            break;
-        case 'Special Item':
-            precio = 18;
-            break;
-        case 'Sale Item':
-            precio = 25;
-            break;
-        case 'Popular Item':
-            precio = 40;
-            break;
-            default: precio = 0;
-            break;
-    }
-    if (((stock > compra) && (compra > 0)) || (nombre == ('Fran' || 'fran' || 'Franco' || 'franco' || 'FRANCO' || 'FRAN'))){
-    console.log('El monto total por el producto es de $' + compra*precio)}
-} */
+
+/* 
+fancyProduct[0].addEventListener('click', () => {agregarAlCarrito('Fancy Product')})
+fancyProduct[1].addEventListener('click', () => {agregarAlCarrito('Fancy Product')})
+specialItem[0].addEventListener('click', () => {agregarAlCarrito('Special Item')})
+specialItem[1].addEventListener('click', () => {agregarAlCarrito('Special Item')})
+saleItem[0].addEventListener('click', () => {agregarAlCarrito('Sale Item')})
+saleItem[1].addEventListener('click', () => {agregarAlCarrito('Sale Item')})
+popularItem[0].addEventListener('click', () => {agregarAlCarrito('Popular Item')})
+popularItem[1].addEventListener('click', () => {agregarAlCarrito('Popular Item')})
+*/
+
+for (const boton1 of fancyProduct){
+    boton1.addEventListener('click', () => {agregarAlCarrito('Fancy Product')})
+}
+for (const boton2 of specialItem){
+    boton2.addEventListener('click', () => {agregarAlCarrito('Special Item')})
+}
+for (const boton3 of saleItem){
+    boton3.addEventListener('click', () => {agregarAlCarrito('Sale Item')})
+}
+for (const boton4 of popularItem){
+    boton4.addEventListener('click', () => {agregarAlCarrito('Popular Item')})
+}
+
 
 function eliminarDelCarrito(producto) {
     let i = carrito.indexOf(producto)
     if (i != -1) {carrito.splice(i, 1)}
 }
+
 
 /* for (const objetoEnCarrito of carrito){
     if (indexOf(objetoEnCarrito) === 0){
